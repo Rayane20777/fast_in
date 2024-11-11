e00d4a5b5c9ea690ebeaf96617097a6ee9c4c225
Rayane Fiach <139344446+Rayane20777@users.noreply.github.com>
2024-11-11 10:55:24 +0100

    first commit

---
+++ src/main/java/com/example/fast_in/FastInApplication.java
@@ -0,0 +1,13 @@
+package com.example.fast_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fast_in/FastInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fast_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
